60bf251481db5ac2af5456ce20688d4b31c3eebb
WangJinzhong <andywang7259@163.com>
2019-06-11 14:14:20 +0800

    to dev800

--- install.sh
+++ install.sh
@@ -3,9 +3,7 @@ rm -rf ~/tmp/vim-backup
 mkdir -p ~/.vim
 mv ~/.vim ~/tmp/vim-backup
 
-# git clone git@github.com:blogsoso/vimfile.git ~/.vim
-
-git clone https://github.com/gialib/vimfile.git ~/.vim
+git clone https://github.com/dev800/vimfile.git ~/.vim
 
 cd ~/.vim
 
